d99040626e221d409e9c848a23218f133383b574
kkh-ssu <kokanghyeon@soongsil.ac.kr>
2026-03-10 13:54:18 +0900

    2주차 hello world

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <string>
+
 int main(void){
-	std::cout<<"helo";
+	std::string name;
+
+	std::cout<<"=== 영화 추천 시스템===\n";
+	std::cout<<"이름 입력: ";
+	getline(std::cin,name);
+	std::cout<<name<<"님 환영합니다\n";
 	return 0;
 }
